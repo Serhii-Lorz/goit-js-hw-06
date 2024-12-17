75652d405e9e7f7db983700f529a641d85a8331d
Serhii Yehorov <egorovs228228@gmail.com>
2024-12-17 13:07:49 +0000

    Complete task-2.js file

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,37 @@
 'use strict';
+
+class Storage {
+  #items
+  constructor(initialItems) {
+    this.#items = Array.isArray(initialItems) ? initialItems : [];
+  };
+
+  getItems() {
+    return this.#items;
+  };
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  };
+
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    } else {
+      return;
+    };
+  };
+};
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
